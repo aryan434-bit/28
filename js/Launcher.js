b377e47c390974beddc46253cb173916f63f173f
aryan434-bit <76521091+aryan434-bit@users.noreply.github.com>
2021-06-30 18:20:22 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,30 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.launcherObject= Constraint.create(options);
+        World.add(world, this.launcherObject);
+    }
+
+    fly(){
+        this.launcherObject.bodyA =null;
+    }
+    attach(body){
+        this.launcherObject.bodyA = body;
+    }
+
+    display(){
+        if(this.launcherObject.bodyA){
+            var pointA = this.launcherObject.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
